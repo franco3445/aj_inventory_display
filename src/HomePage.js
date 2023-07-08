7235b7de107706194d302e9f7addb36186098fab
gFranco <gfranco@velocitymsc.com>
2023-07-07 20:21:44 -0500

    Base Files

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomePage-header">
+
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;
