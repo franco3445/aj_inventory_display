57d4105d826f274fed44a3f8bd744abf6a796636
gFranco <gfranco@velocitymsc.com>
2023-07-10 14:51:30 -0500

    add dummy info

---
+++ src/EquipmentList.tsx
@@ -0,0 +1,22 @@
+export const equipmentListArray = [
+    {
+        id: 1,
+        title: "2018 Mec Scissor Lift",
+        location: "Krum, TX",
+        serialNumber: 504940,
+        dateUploaded: "07/07/2023",
+        image: require('./img/mecScissor.png'),
+        shortAboutMe: "Direct Electric Drive for Exceptional Duty Cycles and Faster Drive Speeds",
+        longAboutMe: "• Standard Integrated Sheet Material Tray Allows 110 lbs. (50 kg) to be Stored and Lifted Outside the Platform • Compact Maneuverability from Front Wheel Drive and Steer with Zero Inside Turn Radius • Fully Proportional Lift and Drive Controls • Full Height Swing Gate • Easy Serviceability with Swing Out Battery and Power Module Trays",
+    },
+    {
+        id: 1,
+        title: "2017 Doosan ForkLift",
+        location: "Decatur, TX",
+        serialNumber: 940504,
+        dateUploaded: "06/06/2023",
+        image: require('./img/forkLift.png'),
+        shortAboutMe: "Doosan’s new 9 Series Order Picker combines innovative safety features, superior performance, and thoughtful ergonomics, making your warehouse operation safer and more efficient.",
+        longAboutMe: "- Auto Directional Alignment (Key on) - Electromagnetic Parking Brake - AC Proportional Lift/Lower Control - Height and Descent Proximity Sensors - Safety Side Gate with Electronic Lockout Function - Battery Side Gate & Hood Cover Safety Interlock - Doosan GSS (Guardian Stability System) > Driving Speed Control > Cornering Speed Control - Rigid High-Stability Mast - Chain Slack Sensor - Emergency Stop Button - Back-Up Alarm",
+    },
+]
